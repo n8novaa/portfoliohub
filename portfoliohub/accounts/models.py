23f8de7626f0179bea046b63c27c2ebb5ff73e25
basil <basilpaul71136@gmail.com>
2026-01-22 15:52:56 +0530

    Add Profile model and register it in admin

--- portfoliohub/accounts/models.py
+++ portfoliohub/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    full_name = models.CharField(max_length=100)
+    headline = models.CharField(max_length=150)
+    bio = models.TextField()
+    skills = models.TextField(help_text="comma-seperated skills")
+    email = models.EmailField()
+    github_url = models.URLField()
+    linkedin_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.full_name
+    
+    
