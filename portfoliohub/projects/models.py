a5c13dee3f43c53b5eec71a2c3774a7f6b288179
basil <basilpaul71136@gmail.com>
2026-01-22 16:36:25 +0530

    Add slug field to project model

--- portfoliohub/projects/models.py
+++ portfoliohub/projects/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     short_description = models.CharField(max_length=255)
     description = models.TextField()
     tech_stack = models.CharField(
@@ -12,6 +13,11 @@ class Project(models.Model):
     live_url = models.URLField(blank=True)
     is_featured = models.BooleanField(default=False)
     created_at = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
