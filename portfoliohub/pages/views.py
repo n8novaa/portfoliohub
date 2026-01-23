bd1475ff17b3b764af2f9bb8f8d94e8c781b8350
basil <basilpaul71136@gmail.com>
2026-01-23 11:02:03 +0530

    Display featured projects on homepage

--- portfoliohub/pages/views.py
+++ portfoliohub/pages/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from accounts.models import Profile
+from projects.models import Project
 
 def home(request):
     profile = Profile.objects.first()
-    return render(request, 'pages/home.html', {'profile': profile})
+    featured_projects = Project.objects.filter(
+        is_featured=True,
+        slug__isnull=False
+        ).exclude(slug='')
+    return render(request, 'pages/home.html', {
+        'profile': profile,
+        'featured_projects' : featured_projects
+        })
